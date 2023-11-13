6a5915c6b34de8346987bb21c1469e49adfb9127
ToontjeM <98271783+ToontjeM@users.noreply.github.com>
2023-11-13 17:31:57 +0100

    Add files via upload

---
+++ resource-rs.tf
@@ -0,0 +1,90 @@
+terraform {
+  required_providers {
+    biganimal = {
+      source  = "EnterpriseDB/biganimal"
+      version = "0.6.1"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "The name of the cluster."
+}
+
+variable "project_id" {
+  type        = string
+  description = "BigAnimal Project ID"
+}
+
+resource "biganimal_cluster" "single_node_cluster" {
+  cluster_name = var.cluster_name
+  project_id   = var.project_id
+
+/*
+  allowed_ip_ranges {
+    cidr_block  = "0.0.0.0/24"
+    description = "localhost"
+  }
+
+  allowed_ip_ranges {
+    cidr_block  = "192.168.0.1/32"
+    description = "description!"
+  }
+*/
+
+  backup_retention_period = "6d"
+  cluster_architecture {
+    id    = "single"
+    nodes = 1
+  }
+  csp_auth = false
+
+  instance_type = "aws:m5.large"
+  password      = resource.random_password.password.result
+  pg_config {
+    name  = "application_name"
+    value = "created through terraform"
+  }
+
+  pg_config {
+    name  = "array_nulls"
+    value = "off"
+  }
+
+  storage {
+    volume_type       = "gp3"
+    volume_properties = "gp3"
+    size              = "4 Gi"
+  }
+
+  pg_type               = "epas"
+  pg_version            = "15"
+  private_networking    = false
+  cloud_provider        = "aws"
+  read_only_connections = false
+  region                = "eu-west-1"
+}
+
+output "password" {
+  sensitive = true
+  value     = resource.biganimal_cluster.single_node_cluster.password
+}
+
+output "faraway_replica_ids" {
+  value = biganimal_cluster.single_node_cluster.faraway_replica_ids
+}
+
+output "connection_uri" {
+  value = biganimal_cluster.single_node_cluster.connection_uri
+}
